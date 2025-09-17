202ec5380fc1df0e82fd99c938e1f77a54714a50
Nihar-Burley <niharburley137@gmail.com>
2025-09-17 22:59:50 +0530

    Resolved merge conflicts

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHERS
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,46 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest activityRequest) {
+        Activity activity =Activity.builder()
+                .userId(activityRequest.getUserId())
+                .type(activityRequest.getType())
+                .duration(activityRequest.getDuration())
+                .caloriesBurned(activityRequest.getCaloriesBurned())
+                .startTime(activityRequest.getStartTime())
+                .additionalMetrics(activityRequest.getAdditionalMetrics())
+                .build();
+
+
+        return mapToResponse(activityRepository.save(activity));
+    }
+
+    private ActivityResponse mapToResponse(Activity activity)
+    {
+        ActivityResponse response=new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+
+        return response;
+    }
+
+}
